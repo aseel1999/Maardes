c578649052670af57729583401bd743b245fd6ef
TouradGithub <95051771+TouradGithub@users.noreply.github.com>
2022-12-15 00:51:01 +0100

    add table in mode Location_maards
    add table in mode Location_maards

--- app/Models/Location_Maared.php
+++ app/Models/Location_Maared.php
@@ -8,8 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Location_Maared extends Model
 {
     use HasFactory;
+    protected $table = 'location_maarads';
+    // protected $table='location__maareds';
     protected $fillable = ['name','area','description','imagemarad'];
     public function user(){
         return $this->belongsTo(User::class);
     }
+
+
+
 }
